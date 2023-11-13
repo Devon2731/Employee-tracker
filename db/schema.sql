f2e9b6a8dee70afb27c9ab8ab11dc2984bce7276
Taz Whitaker <tazwhitaker@Tazs-MacBook-Air.local>
2023-11-13 14:47:49 -0500

    final update

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,11 @@
-DROP DATABASE IF EXISTS employee_db;
-CREATE DATABASE employee_db;
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
 
 USE employees_db;
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
@@ -23,9 +23,8 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
-    manager_id INT,
-    FOREIGN KEY (role_id) REFERENCES employee(id) ON DELETE SET NULL,
-    FOREIGN KEY (manager_id)
-    REFERENCES employee(id)
-    ON DELETE SET NULL
-);
+    manager_id INT REFERENCES employee(id) ON DELETE SET NULL,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL   
+)
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,22 +1,21 @@
 INSERT INTO department (department_name)
 VALUES ("Executive"),
-    ("Sales"),
-    ("Engineering"),
-    ("Finance"),
-    ("QA");
+       ("Finance"),
+       ("Sales"),
+       ("Engineering"),
+       ("QA");
 
-INSERT INTO role (title, salary, department_id)
-VALUES ("CEO", 1000000, 1),
-    ("Sr.Accountant", 100000,2)
-    ("Mech Engineer", 90000,4),
-    ("Jr. QA", 70000, 5),
-    ("Sales Manager", 90000, 3);
-    ("Enigeerung Manger", 220000,4);
+INSERT INTO role (title,salary,department_id) 
+VALUES  ('CEO',1000000,1),
+        ('Sr.Accountant',100000,2),
+        ('Mech Engineer',90000,4),
+        ('Jr.QA',70000,5),
+        ('Sales Manager',90000,3),
+        ('Engineering Manager',220000,4);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("John", "Doe", 1, NULL),
-    ("Mike", "Chan", 2, 1),
-    ("Ashley", "Rodriguez", 3, 1);
-  
+INSERT into employee (first_name,last_name,role_id,manager_id)
+VALUES ('John','Doe',1,NULL),
+       ('Mike','Chan',2,1),
+        ('Becky','Vicky',3,1);
 
     
