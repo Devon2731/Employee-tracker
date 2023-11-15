67e9ca435acec08e63334026eb2a851f7457a28a
Taz Whitaker <tazwhitaker@Tazs-MacBook-Air.local>
2023-11-14 21:04:04 -0500

    updated new coding

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,19 +3,25 @@ VALUES ("Executive"),
        ("Finance"),
        ("Sales"),
        ("Engineering"),
-       ("QA");
+       ("Legal");
 
 INSERT INTO role (title,salary,department_id) 
 VALUES  ('CEO',1000000,1),
         ('Sr.Accountant',100000,2),
         ('Mech Engineer',90000,4),
-        ('Jr.QA',70000,5),
+        ('Lawyer',70000,5),
         ('Sales Manager',90000,3),
-        ('Engineering Manager',220000,4);
+        ('Salesperson',60000,3),
+        ('Lead Engineer',220000,4);
 
 INSERT into employee (first_name,last_name,role_id,manager_id)
-VALUES ('John','Doe',1,NULL),
-       ('Mike','Chan',2,1),
-        ('Becky','Vicky',3,1);
+VALUES ('John','Doe',1,),
+       ('Mike','Chan',2,5),
+        ('Becky','Vicky',3,4)
+        ('Jane','Doe',4,2),
+        ('Oscar','Gomez',5,3),
+        ('Kevin','Tupik',6,3),
+        ('Ashley','Rodriguez',7,1)
+        ('Kunal','Singh',8,5);
 
     
--- server.js
+++ server.js
@@ -1,281 +1,86 @@
-
-// Importing necessary npm packages
 const inquirer = require('inquirer');
-const cTable = require('console.table');
-
-var process = require('process');
-
-//importing the class and methods for department, role and Employee 
-const Department = require ('./lib/department');
+const Department = require('./lib/department');
 const Role = require('./lib/role');
 const Employee = require('./lib/employee');
 
-//Array of initial  menuQuestions: 
-const menuQuestions =
-    {
-        message: "What would you like to do?",
-        name: "optedAction",
-        type: "list",
-        choices: ["view all departments", 
-                  "add a new department",
-                  
-                  "view all roles",
-                  "add a new role",
-                  "delete a role",
-                  
-                  "view all employees",
-                  "add a new employee",
+async function mainPrompt() {
+  try {
+    const answers = await inquirer.prompt({
+      message: "What would you like to do?",
+      name: "likeToDo",
+      type: "list",
+      choices: [
+        "view all departments",
+        "add a new department",
+        "view all roles",
+        "add a new role",
+        "view all employees",
+        "add a new employee",
+        "update an employee role",
+        "update an employee manager",
+        "delete an employee",
+        "delete a role",
+        "delete a department",
+        "all done!!"
+      ],
+      filter(val) {
+        return val.toLowerCase();
+      }
+    });
 
-                  "all done!!"
-                 ],
-        filter(val) {
-          return val.toLowerCase();
-        }
+    switch (answers.likeToDo) {
+      case 'view all departments':
+        await displayAllDepts();
+        break;
+      case 'view all roles':
+        await displayAllRoles();
+        break;
+      case 'view all employees':
+        await displayAllEmployees();
+        break;
+      case 'all done!!':
+        console.log('Hope you loved using employee manager!!');
+        process.exit(0);
+      default:
+        console.log('Invalid option. Please choose a valid option.');
     }
+  } catch (error) {
+    console.error('Error in main prompt:', error.message);
+    process.exit(1);
+  }
 
-// initiate the program with main prompt
-async function init() {
-    await inquirer.prompt(menuQuestions)
-    .then((answers) => {
-        switch(answers.optedAction){
-            case 'view all departments':
-                displayAllDepts();
-                break;
-            case 'view all roles':
-                displayAllRoles();
-                break;
-            case 'view all employees':
-                displayAllEmployees();
-                break;
-            case 'add a new department':
-                addNewDepartment();
-                break;
-            case 'add a new role':
-                addNewRole(); 
-                break;
-            case 'add a new employee':
-                addNewEmployee();
-                break;
-            case 'delete a role':
-                deleteRole();
-                break;
-            default:
-                console.log('Hope you loved using employee manager!!')
-                process.exit(0);
-        }
-        return;
-    })
+  return mainPrompt(); // Consider using a loop instead of recursion
 }
 
-// Function to display all Departments
-async function displayAllDepts(){
-    const newDept = new Department();
-    const displayDept = await newDept.deptSelectAll();
-    console.log ('\n *** LIST OF DEPARTMENTS ***\n');
-    console.table(displayDept);
-    init();
-    return;
+async function displayAllDepts() {
+  try {
+    const depts = await Department.deptSelectAll();
+    console.log('\n *** LIST OF DEPARTMENTS *** \n');
+    console.table(depts);
+  } catch (err) {
+    console.error("Error while fetching and displaying departments", err.message);
+  }
 }
 
-// Function to display all Roles
 async function displayAllRoles() {
-    const roleSelect = new Role();
-    const allroles = await roleSelect.roleSelectAll();
-    console.log ('\n *** LIST OF ROLES ***\n');
-    console.table(allroles);
-    init();
-    return;
+  try {
+    const roles = await Role.roleSelectAll();
+    console.log('\n *** LIST OF ROLES ***\n');
+    console.table(roles);
+  } catch (error) {
+    console.error('Error while fetching and displaying roles:', error.message);
+  }
 }
 
-// Function to display all employees
-async function displayAllEmployees (){
-    const employeeSelect = new Employee();
-    const displayEmployees = await employeeSelect.empSelectAll();
-    console.log ('\n *** LIST OF EMPLOYEEES ***\n');
-    console.table(displayEmployees);
-    init();
-    return;
+async function displayAllEmployees() {
+  try {
+    const employees = await Employee.empSelectAll();
+    console.log('\n *** LIST OF EMPLOYEES ***\n');
+    console.table(employees);
+  } catch (error) {
+    console.error('Error while fetching and displaying employees:', error.message);
+  }
 }
 
-
-// Functions to add new department 
-async function addNewDepartment() {
-    const addNewDeptQues = {
-        message: "Enter the department name:",
-        name: "deptName",
-        type: "input"
-    }
-    await inquirer.prompt(addNewDeptQues)
-    .then((answers) => {
-        const deptDetails = new Department(answers.deptName);
-        insertDepartment(deptDetails);
-    });
-    return;
-}
-
-async function insertDepartment(deptDetails) {
-    await deptDetails.deptInsert();
-    init();
-    return;
-}
-
-// Functions to add new role  
-async function addNewRole(){
-    // Prepare a list of department 
-    const newDept = new Department();
-    const displayDept = await newDept.deptSelectAll();
-    const deptList = function (displayDept) {
-    const listDept = [];
-    displayDept.forEach(element => {
-        listDept.push(element.id + '.' + element.department_name); 
-    });
-    return listDept;  
-    }
-    
-    const roleQuestions = [
-        {
-        message: "Enter the role title:",
-        name: "roleTitle",
-        type: "input"
-        },
-        {
-        message: "Enter the salary for the role:",
-        name: "roleSalary",
-        type: "input"
-        },
-        {
-            message: "Select a department:",
-            name: "deptName1",
-            type: "list",
-            choices: deptList(displayDept)
-        }
-    ]
-    await inquirer.prompt(roleQuestions)
-    .then((answers) => {
-        const deptIDSeparator = answers.deptName1.split(".");
-        const deptID = Number(deptIDSeparator[0]);
-        const roleSal = parseFloat(answers.roleSalary);
-        const roleDetails = new Role(answers.roleTitle, roleSal, deptID);
-        insertRole(roleDetails);
-    });
-    return;
-}
-
-
-async function insertRole(roleDetails) {
-    await roleDetails.roleInsert();
-    init();
-    return;
-}
-
-//// Functions to add new Employee  
-async function addNewEmployee(){
-    //prepare list of roles 
-    const newRole = new Role();
-    const displayRole = await newRole.roleSelectAll();
-    const roleList = function (displayRole) {
-        const listRoles = [];
-        displayRole.forEach(element => {
-            listRoles.push(element.id + '.' + element.title); 
-        });
-        return listRoles;  
-    }
-
-    //prepare list of Managers
-
-    const newManager = new Employee();
-    const displayManagers = await newManager.empSelectAll();
-    const managerList = function (displayManagers) {
-        const listManagers = [];
-        listManagers.push('0.None');
-        displayManagers.forEach(element => {
-            listManagers.push(element.id + '.' + element.first_name + ' ' + element.last_name); 
-        });
-        return listManagers;  
-    }
-
-    //Employee questions
-    const employeeQuestions = [
-        {
-        message: "Enter the first name:",
-        name: "fname",
-        type: "input"
-        },
-        {
-            message: "Enter the last name:",
-            name: "lname",
-            type: "input"
-        },
-        {
-        message: "Enter the role id:",
-        name: "roleID",
-        type: "list",
-        choices: roleList(displayRole)
-        },
-        {
-            message: "Select employee's manager:",
-            name: "mgrName",
-            type: "list",
-            choices: managerList(displayManagers)
-        }
-    ]
-
-    await inquirer.prompt(employeeQuestions)
-    .then((answers) => {
-        let empDetails;
-        const roleIDSeparator = answers.roleID.split(".");
-        const roleID = Number(roleIDSeparator[0]);
-
-        const mgrIDSeparator = answers.mgrName.split(".");
-        const mgrID = Number(mgrIDSeparator[0]);
-
-        if (mgrID > 0) {
-            empDetails = new Employee(answers.fname,answers.lname,roleID,mgrID);
-        } else {
-            empDetails = new Employee(answers.fname,answers.lname,roleID, null);
-        }
-        insertEmployee(empDetails);
-    });
-    return;
-}
-
-
-async function insertEmployee(empDetails) {
-    await empDetails.empInsert();
-    init();
-    return;
-}
-
-
-async function deleteRole(){
-    //prepare list of roles 
-    let roleID;
-    const newRole = new Role();
-    const displayRole = await newRole.roleSelectAll();
-    const roleList = function (displayRole) {
-        const listRoles = [];
-        displayRole.forEach(element => {
-            listRoles.push(element.id + '.' + element.title); 
-        });
-        return listRoles;  
-    }
-
-    const delRoleQues = {
-        message: "Enter the role to be deleted:",
-        name: "roleID",
-        type: "list",
-        choices: roleList(displayRole)
-    }
-
-    await inquirer.prompt(delRoleQues)
-    .then((answers) => {
-        const roleIDSeparator = answers.roleID.split(".");
-        roleID = Number(roleIDSeparator[0]);
-    });
-    const roleDetails = new Role();
-    await roleDetails.roleDelete(roleID);
-    init();
-}
-
-// Initiate the program with main set of prompts
-init();
+// Start the program with the main prompt
+mainPrompt();
